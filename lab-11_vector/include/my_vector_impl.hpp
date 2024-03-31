e6b8d716d1f572777b2a9ecad14ab9843ef81048
Alexandr Isaenkov <isaenkovcc@mail.ru>
2024-04-01 02:27:39 +0300

    upd

--- lab-11_vector/include/my_vector_impl.hpp
+++ lab-11_vector/include/my_vector_impl.hpp
@@ -14,8 +14,8 @@ namespace help_functions
         {
             return n;
         }
-        int count = 0;
-        while (n)
+        std::size_t count = 0;
+        while (n != 0)
         {
             n >>= 1;
             ++count;
