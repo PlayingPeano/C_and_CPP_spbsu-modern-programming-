ae22401ba976557027c01dbe2e20e53a80dac39b
Alexandr Isaenkov <isaenkovcc@mail.ru>
2024-04-01 10:49:03 +0300

    upd

--- lab-11_vector/include/my_vector_impl.hpp
+++ lab-11_vector/include/my_vector_impl.hpp
@@ -120,8 +120,8 @@ namespace containers
         if (n > capacity_)
         {
             my_vector<T> copiedArray(return_copy_of_my_vector_and_delete_array());
-            array_ = (T*)(new char[(n * sizeof(T))]);
             capacity_ = help_functions::upper_bound_by_power_of_two(n);
+            array_ = (T*)(new char[(capacity_ * sizeof(T))]);
             size_ = copiedArray.size_;
             for (std::size_t i = 0; i < copiedArray.size_; ++i)
             {
