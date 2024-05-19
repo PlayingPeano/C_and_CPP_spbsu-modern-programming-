a434b5687ad6b3c6bb15880c8358b140da6faebd
Alexandr Isaenkov <isaenkovcc@mail.ru>
2024-05-19 15:32:58 +0300

    fix

--- lab-14_linq/src/test.cpp
+++ lab-14_linq/src/test.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
+#include "linq.hpp"
+#include <cassert>
+#include <vector>
 
+using linq::from;
 
-int main(int argc, char** argv)
+void from_select() {
+    const std::size_t xs[] = { 1, 2, 3, 4, 5 };
+    std::vector<int> res = from(xs, xs + 3).select<int>([](int x) { return x - 5; }).to_vector();
+    std::vector<int> expected = { -4, -3, -2 };
+    assert(res == expected);
+}
+
+int main(int argc, char* argv[])
 {
-
+    from_select();
     return EXIT_SUCCESS;
 }
