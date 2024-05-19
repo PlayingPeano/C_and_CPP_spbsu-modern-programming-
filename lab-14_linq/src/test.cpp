37ecb042490afc11277263d7dd5213a21efef21f
Alexandr Isaenkov <isaenkovcc@mail.ru>
2024-05-19 18:19:53 +0300

    cleaning

--- lab-14_linq/src/test.cpp
+++ lab-14_linq/src/test.cpp
@@ -4,15 +4,7 @@
 
 using linq::from;
 
-void from_select() {
-    const std::size_t xs[] = { 1, 2, 3, 4, 5 };
-    std::vector<int> res = from(xs, xs + 3).select<int>([](int x) { return x - 5; }).to_vector();
-    std::vector<int> expected = { -4, -3, -2 };
-    assert(res == expected);
-}
-
 int main(int argc, char* argv[])
 {
-    from_select();
     return EXIT_SUCCESS;
 }
